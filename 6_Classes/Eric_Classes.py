302c7ea6e6a5cbfa2fe2b858903e1610fb188faf
Jim Johnson <email_programmer_jim@yahoo.com>
2026-05-09 15:46:29 -0500

    Saving update

--- 6_Classes/Eric_Classes.py
+++ 6_Classes/Eric_Classes.py
@@ -2,6 +2,6 @@ class Creeper:
     def __init__(self, name):  #constructor
         self.name   = name
 
-creeper1 = Creeper("Eric") #Creeper()
+creeper1 = Creeper("Obi wan Kenobi") #Creeper()
 
 print(creeper1.name)    # → Eric
