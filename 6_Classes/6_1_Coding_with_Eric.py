dbe2736e27a6262c08368c935930db177fe41d23
Jim Johnson <email_programmer_jim@yahoo.com>
2026-05-16 14:40:19 -0500

    saving  for eric

--- 6_Classes/6_1_Coding_with_Eric.py
+++ 6_Classes/6_1_Coding_with_Eric.py
@@ -1,8 +1,16 @@
-class Eric:
-    def __init__(self, name, health):
-        self.name = name
-        self.health = health
+# class Eric:
+#     def __init__(self, name, health):
+#         self.name = name
+#         self.health = health
 
-eric_instance = Eric("Eric the Warrior", 75)
-print(eric_instance.name)  # → Erice the Warrior
-print(eric_instance.health)  # → 75
+# eric_instance = Eric("Eric the Warrior", 75)
+# print(eric_instance.name)  # → Erice the Warrior
+# print(eric_instance.health)  # → 75
+
+class Creeper: 
+  def __init__(self, name: str, hp: int): #constructor 
+    self.name = name 
+    self.hp = hp 
+    
+creeper1 = Creeper("explosion", 10) #Creeper() 
+print(creeper1.name + " has " + str(creeper1.hp)) # → Eric
