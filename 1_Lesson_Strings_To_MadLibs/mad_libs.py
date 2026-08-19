829f8c9e2b1163a6dcce3eb6741f1cfae9d6f668
Jim Johnson <email_programmer_jim@yahoo.com>
2026-08-18 20:53:41 -0500

    Working with Eric to save mad libs.

---
+++ 1_Lesson_Strings_To_MadLibs/mad_libs.py
@@ -0,0 +1,42 @@
+def mad_libs_welcome():
+    print("=== WELCOME TO MAD LIBS! ===")
+    print("Answer each question with ANY word - the sillier the better!")
+    print("")
+
+a_name = input("Type a funny name: ")
+an_animal = input("Type an animal: ")
+a_body_part = input("Type a body part: ")
+an_adjective1 = input("Type a describing word (e.g. slimy, purple, tiny): ")
+an_adjective2 = input("Type another describing word: ")
+a_verb1 = input("Type an action word ending in -ed (e.g. jumped, exploded, wiggled): ")
+a_verb2 = input("Type another action word ending in -ing (e.g. running, burping, flying): ")
+a_place = input("Type a place (e.g. school, the moon, a taco shop): ")
+a_food = input("Type a food: ")
+a_number = input("Type a number: ")
+an_exclamation = input("Type a silly exclamation (e.g. YIKES, ZOINKS, OH BEANS): ")
+
+print("")
+print("============================================")
+print("        ** YOUR MAD LIBS STORY **          ")
+print("============================================")
+print("")
+print("One " + an_adjective1 + " morning, " + a_name + " woke up to find a " + an_adjective2 + " " + an_animal)
+print("sitting right on their " + a_body_part + "!")
+print("")
+print(an_exclamation + "! screamed " + a_name + ", leaping out of bed and " + a_verb2 + " all the way to " + a_place + ".")
+print("")
+print("When they arrived, they discovered " + a_number + " plates of " + a_food + " stacked on the floor.")
+print("The " + an_animal + " had followed them and immediately " + a_verb1 + " right into the biggest pile!")
+print("")
+print("Everyone at " + a_place + " laughed so hard that their " + a_body_part + " fell off.")
+print("")
+print(a_name + " just shook their head and said,")
+print('"I can\'t believe a ' + an_adjective2 + " " + an_animal + " ruined my " + a_food + " again. " + an_exclamation + '!"')
+print("")
+print("                    THE END")
+print("============================================")
+
+
+#run the mad_libs_variables function to start the game
+mad_libs_welcome()
+
